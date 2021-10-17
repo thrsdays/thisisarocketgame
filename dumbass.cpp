f0ddfa784963e2eda5c35e723878f27a62f05134
E <emranpoh@gmail.com>
2021-10-17 11:32:47 +0800

    basic sfml test

---
+++ dumbass.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int age;
+  //cout needs namepsace std;
+  //cout << "Hello Bitches this is my first C++ Program yallz\n";
+  cout << "hi" << "bye\n";
+  cin >> age;
+  cout << age << "\n";
+  return 0; 
+}
---
+++ helloworld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Hello Bitches this is my first C++ Program yallz\n";
+  return 0;
+}
